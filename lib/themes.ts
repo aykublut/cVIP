77e3ddd983924871905d3c0ac104a1e942ec0dd3
Aykutak04 <aykutak.04.55@gmail.com>
2026-04-27 00:28:15 +0300

    ıjdıwjd

--- lib/themes.ts
+++ lib/themes.ts
@@ -4,20 +4,20 @@
 export const THEMES = [
   {
     id: "modern-split",
-    name: "Modern Çift Kolon",
+    name: "cVIP",
     description:
       "Sol tarafta koyu bir sütun ile şık ve profesyonel bir görünüm.",
   },
-  {
-    id: "minimalist-center",
-    name: "Minimalist Merkez",
-    description: "Zarif tipografi ile tek sütunlu, ferah ve temiz bir yapı.",
-  },
-  {
-    id: "default-theme",
-    name: "vip kalite",
-    description: "vip",
-  },
+  // {
+  //   id: "minimalist-center",
+  //   name: "Minimalist Merkez",
+  //   description: "Zarif tipografi ile tek sütunlu, ferah ve temiz bir yapı.",
+  // },
+  // {
+  //   id: "default-theme",
+  //   name: "vip kalite",
+  //   description: "vip",
+  // },
 ];
 
 export type ThemeId = (typeof THEMES)[number]["id"];
